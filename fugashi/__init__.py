d652659e8bde40617270bc640f5abe7c4e77b51b
Yohei Tamura <tamuhey@gmail.com>
2021-01-26 21:23:57 +0900

    add __all__ for static analysis

--- fugashi/__init__.py
+++ fugashi/__init__.py
@@ -1,2 +1,12 @@
 from .fugashi import *
 
+__all__ = [
+    "GenericTagger",
+    "Node",
+    "Tagger",
+    "UnidicFeatures17",
+    "UnidicFeatures26",
+    "UnidicFeatures29",
+    "UnidicNode",
+    "create_feature_wrapper",
+]
